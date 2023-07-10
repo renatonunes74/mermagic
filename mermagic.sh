31a2adcaa9f3c17464eeabbc188694fa67ab3816
renatonunes74 <renato.s.nunes74@gmail.com>
2023-07-09 21:57:36 -0300

    refactor: formatar a METADATA invez de cada classe

--- mermagic.sh
+++ mermagic.sh
@@ -1,28 +1,35 @@
-# class=$(find . -name "*.class" -print0 | xargs -0 javap)
-METADATA=$(cat mermaid.md)
-CLASS=$(echo "$METADATA" | grep "Compiled from" | sed -e 's/Compiled"//;s/\.java"//')
-echo -e "\`\`\`mermaid\nclassDiagram";
-for i in $CLASS; do
-	echo "$METADATA" |
-	sed -e 's/public.*interface.*/<<interface>>/' |
-		sed -n '/'"$i.java"'/,/\}/p' |
-		grep -v "{" |
-		sed -e 's/Compiled from "/class /' |
-		sed -e 's/\.java"/ \{/' |
-		sed -e 's/private /-/' |
+METADATA=$(find $1 -name "*.class" -print0 | xargs -0 javap -p |
+	# Formatando a metadata 
+	# Visibilidade dos atributos
+	sed -e 's/private /-/' |
 		sed -e 's/public /+/' |
 		sed -e 's/protected /#/' |
-		# Verificar
-		sed -e 's/ final //g' |
-		sed -e 's/java.lang.//g' |
-		sed -e 's/org.springframework.//g' |
-		sed -e 's/http.ResponseEntity //g'
-	done
-	# Relação das classes
-	# interface
-	INTERFACE=$(echo "$METADATA" | grep "interface") 
-	echo "$INTERFACE" | while IFS= read -r line; do
-	echo $(echo "$line" | sed -e 's/.*\.\([^ ]*\) extends.*/\1 ..|>/'; echo "$line" | sed -e 's/.*\.\([^ ,]*\),.*/\1 : Realization/')
-done
-
-echo "\`\`\`"
+		sed -e 's/package /~/' |
+		# Atributos
+			sed -e 's/Integer /Integer : /' |
+				sed -e 's/void /void : /' |
+				sed -e 's/String /String : /' |
+				sed -e 's/boolean /boolean : /' |
+				sed -e 's/int /int : /' |
+				# Verificar
+							sed -e 's/ final //g' |
+								sed -e 's/java.lang.//g' |
+								sed -e 's/org.springframework.//g' |
+								sed -e 's/http.ResponseEntity //g'
+							)
+							# Verifica em cada classe e formata os métodos
+							CLASS=$(echo "$METADATA" | grep "Compiled from" | sed -e 's/Compiled from "//;s/\.java"//')
+							echo -e "\`\`\`mermaid\nclassDiagram";
+							for i in $CLASS; do
+								echo "$METADATA" |
+									sed -e 's/public.*interface.*/<<interface>>/' |
+									sed -n '/'"$i.java"'/,/\}/p' |
+									grep -v "{" |
+									sed -e 's/Compiled from "/class /' |
+									sed -e 's/\.java"/ \{/'
+								done
+								INTERFACE=$(echo "$METADATA" | grep "interface") 
+								echo "$INTERFACE" | while IFS= read -r line; do
+								echo $(echo "$line" | sed -e 's/.*\.\([^ ]*\) extends.*/\1 ..|>/'; echo "$line" | sed -e 's/.*\.\([^ ,]*\),.*/\1 : Realization/')
+							done
+							echo "\`\`\`"
